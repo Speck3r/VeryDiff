35a0244a38d23984d1538b6a4ed59fea18e0bfc4
phK3 <ufedz@student.kit.edu>
2024-12-02 18:31:22 +0100

    make usage example self contained

--- usage_examples.jl
+++ usage_examples.jl
@@ -2,8 +2,8 @@
 using VeryDiff, LinearAlgebra, VNNLib, Optim
 import VeryDiff: load_polynomial_nn, load_approximation_bounds
 
-model_file = "../vnncomp2022_benchmarks/benchmarks/mnist_fc/onnx/mnist-net_256x6.onnx"
-poly_dir  = "../PolynomialEquivalenceNN/src/poly_coeffs/models/mnist_fc/poly_2"
+model_file = "./test/examples/networks/mnist-net_256x6.onnx"
+poly_dir = "./test/examples/poly_coeffs/models/mnist_fc/poly_2/"
 
 nn = VNNLib.load_network(model_file)
 nn_poly = load_polynomial_nn(model_file, poly_dir);
@@ -20,10 +20,25 @@ ẑ_poly_bnds = nn_poly(z, PropState(true), bounds)
 
 # propagate differential zonotope through the difference network
 ∂z = VeryDiff.Zonotope(zeros(784, 784), zeros(784), nothing)
-# differential zonotope is consists of zono for the first net, zono for the second net and zono for the difference
+# differential zonotope consists of zono for the first net, zono for the second net and zono for the difference
 zΔ = VeryDiff.DiffZonotope(z, deepcopy(z), ∂z, 0, 0, 0)
 
+tmp = VeryDiff.TIGHTEN_BOUNDS_DIFF[]
+# just use individual network bounds
+VeryDiff.TIGHTEN_BOUNDS_DIFF[] = false
 ẑΔ = nn_diff(zΔ, PropState(true))
+
+# now use the differential bounds to tighten the individual network bounds 
+VeryDiff.TIGHTEN_BOUNDS_DIFF[] = true
+ẑΔ_tighten = nn_diff(zΔ, PropState(true))
+
+# restore previous value 
+VeryDiff.TIGHTEN_BOUNDS_DIFF[] = tmp
+
+# now use approximation bounds for the polynomial network
+# !!! this leads to an error because the stored bounds don't intersect with the actual bounds !!!
+ẑΔ_bounds = nn_diff(zΔ, PropState(true), bounds, nothing)
+
 
 
 # find a good estimate on the lower bound of the difference of the nns 
@@ -37,6 +52,8 @@ end
 x₀ = zeros(784)
 res = optimize(optfun, -0.05 .* ones(784), 0.05 .* ones(784), x₀, SAMIN(), 
                Optim.Options(show_trace=true, show_every=1000, iterations=10000))
+
+
 
 
 

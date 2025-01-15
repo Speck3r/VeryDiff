125d680fb607cd2aaea097fb738ed54b150d7c78
phK3 <ufedz@student.kit.edu>
2025-01-15 14:10:11 +0100

    adapted fuzzer with nice polynomial networks

--- test/DiffPolyFuzzing.jl
+++ test/DiffPolyFuzzing.jl
@@ -28,8 +28,66 @@ function get_weighted_random_vector1(values, weights::Vector{Int},size)
     return result
 end
 
+
+function construct_feasibility_model(Z, x_in, y1, y2)
+    input_dim = length(x_in)
+    Z₁ = Z.Z₁
+    Z₂ = Z.Z₂
+    ∂Z = Z.∂Z
+    model = Model(() -> Gurobi.Optimizer(GRB_ENV))
+    set_optimizer_attribute(model, "OutputFlag", 0)
+    @variable(model, -1 <= lp_x[1:size(∂Z.G,2)] <= 1)
+    @constraint(model, lp_x[1:input_dim] .== x_in)
+    @constraint(model, ∂Z.G*lp_x .+ ∂Z.c .== y1.-y2)
+    @constraint(model, Z₁.G*lp_x[1:size(Z₁.G,2)] .+ Z₁.c .== y1)
+    offset = size(Z₁.G,2)+1
+    @constraint(model, 
+        Z₂.G[:,1:input_dim]*lp_x[1:input_dim] .+
+        Z₂.G[:,input_dim+1:end]*lp_x[offset:offset+Z.num_approx₂-1] .+
+        Z₂.c .== y2)
+    
+    return model
+end
+
+
+function construct_maxvio_model(Z, x_in, y1, y2; bound_max_vio=false, model=nothing)
+    input_dim = length(x_in)
+    Z₁ = Z.Z₁
+    Z₂ = Z.Z₂
+    ∂Z = Z.∂Z
+    if isnothing(model)
+        model = Model(() -> Gurobi.Optimizer(GRB_ENV))
+    end
+    
+    #set_optimizer_attribute(model, "OutputFlag", 0)
+    @variable(model, -1 <= lp_x[1:size(∂Z.G,2)] <= 1)
+    @variable(model, v[1:(size(∂Z.G, 1) + size(Z₁.G, 1) + size(Z₂.G, 1))])
+    @variable(model, v_max)
+    @constraint(model, lp_x[1:input_dim] .== x_in)
+    @constraint(model, ∂Z.G*lp_x .+ ∂Z.c .- (y1 .- y2) .== v[1:size(∂Z.G, 1)])
+    @constraint(model, Z₁.G*lp_x[1:size(Z₁.G,2)] .+ Z₁.c .- y1 .== v[(size(∂Z.G, 1)+1):(size(∂Z.G, 1) + size(Z₁.G, 1))])
+    offset = size(Z₁.G,2)+1
+    @constraint(model, 
+        Z₂.G[:,1:input_dim]*lp_x[1:input_dim] .+
+        Z₂.G[:,input_dim+1:end]*lp_x[offset:offset+Z.num_approx₂-1] .+
+        Z₂.c .- y2 .== v[(size(∂Z.G, 1) + size(Z₁.G, 1) + 1):end])
+    
+    @constraint(model, v_max .>= v)
+    @constraint(model, v_max .>= .-v)
+
+    if bound_max_vio
+        # add constraint to debug unbounded model.
+        @constraint(model, v_max .>= -100000)
+    end
+
+    @objective(model, Min, v_max)
+
+    return model
+end
+
 @timeit VeryDiff.to "Fuzzing" begin
 NET_COUNT = 0
+error_net = nothing
 while true
     global NET_COUNT
     next_seed = rand(1:9999)
@@ -87,11 +145,20 @@ while true
         N2 = Network(deepcopy(layers2))
         #push!(networks,(N1,N2,new_dim))
         degree = rand(2:5)
-        coeff1 = rand(Float64,(new_dim,degree+1))
-        #coeff2 = randn(Float64,(new_dim,degree+1))
-        push!(layers1, Poly(coeff1))
-        push!(layers2, ReLU())
+
+        # TODO: nice polynomial networks 
+        push!(layers1, ReLU())
         N1 = Network(deepcopy(layers1))
+        # just use 5 as the width (the largest width below is 4 plus the offset can by anything in [0, 1])
+        z = Zonotope(Matrix(5*I,input_dim,input_dim),zeros(input_dim),nothing)
+        N1 = approximate_polynomial_iterative(N1, z, degree)
+
+
+        #coeff1 = rand(Float64,(new_dim,degree+1))
+        #coeff2 = randn(Float64,(new_dim,degree+1))
+        #push!(layers1, Poly(coeff1))
+        push!(layers2, ReLU())
+        #N1 = Network(deepcopy(layers1))
         N2 = Network(deepcopy(layers2))
         push!(networks,(N1,N2,new_dim))
     end
@@ -122,23 +189,34 @@ while true
             x = Zin.G*x_in + Zin.c
             y1 = N1(x)
             y2 = N2(x)
-            model = Model(() -> Gurobi.Optimizer(GRB_ENV))
-            set_optimizer_attribute(model, "OutputFlag", 0)
-            @variable(model, -1 <= lp_x[1:size(Z.∂Z.G,2)] <= 1)
-            @constraint(model, lp_x[1:input_dim] .== x_in)
-            @constraint(model, Z.∂Z.G*lp_x .+ Z.∂Z.c .== y1.-y2)
-            @constraint(model, Z.Z₁.G*lp_x[1:size(Z.Z₁.G,2)] .+ Z.Z₁.c .== y1)
-            offset = size(Z.Z₁.G,2)+1
-            @constraint(model, 
-                Z.Z₂.G[:,1:input_dim]*lp_x[1:input_dim] .+
-                Z.Z₂.G[:,input_dim+1:end]*lp_x[offset:offset+Z.num_approx₂-1] .+
-                Z.Z₂.c .== y2)
+            #model = Model(() -> Gurobi.Optimizer(GRB_ENV))
+            #set_optimizer_attribute(model, "OutputFlag", 0)
+            #@variable(model, -1 <= lp_x[1:size(Z.∂Z.G,2)] <= 1)
+            #@constraint(model, lp_x[1:input_dim] .== x_in)
+            #@constraint(model, Z.∂Z.G*lp_x .+ Z.∂Z.c .== y1.-y2)
+            #@constraint(model, Z.Z₁.G*lp_x[1:size(Z.Z₁.G,2)] .+ Z.Z₁.c .== y1)
+            #offset = size(Z.Z₁.G,2)+1
+            #@constraint(model, 
+            #    Z.Z₂.G[:,1:input_dim]*lp_x[1:input_dim] .+
+            #    Z.Z₂.G[:,input_dim+1:end]*lp_x[offset:offset+Z.num_approx₂-1] .+
+            #    Z.Z₂.c .== y2)
+            #model = construct_feasibility_model(Z, x_in, y1, y2)
+            model = construct_maxvio_model(Z, x_in, y1, y2)
             optimize!(model)
-
+            
             error = false
-            if termination_status(model) == MOI.INFEASIBLE
+            if ~is_solved_and_feasible(model) # termination_status(model) == MOI.INFEASIBLE
                 println("Solver Status: ", termination_status(model))
-                error =true
+                print(solution_summary(model))
+                error_net = N
+
+                print(model)
+
+                error = true
+            elseif objective_value(model) > 0
+                println("Optimal Solution: ", objective_value(model))
+                error_net = N 
+                error = true 
             end
             # println("Solver Status: ", termination_status(model))
 
@@ -163,6 +241,7 @@ while true
                     error = true
                 end
                 if error
+                    @show k
                     throw("Found error")
                 end
                 

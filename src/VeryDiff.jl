c74c863db78554e9ce3726c84df229c362733a70
Philipp Kern <philipp.kern@kit.edu>
2025-03-26 22:36:43 +0100

    fallback to GLPK if no Gurobi

--- src/VeryDiff.jl
+++ src/VeryDiff.jl
@@ -13,6 +13,7 @@ using PolynomialRoots
 using Optim
 
 using GLPK
+using Gurobi
 
 NEW_HEURISTIC = true
 USE_GUROBI = true
@@ -37,15 +38,21 @@ const OPTIM_ITERS = Ref{Int}(10)
 # We have our own multithreadding so we don't want to use BLAS multithreadding
 function __init__()
     BLAS.set_num_threads(1)
-    GRB_ENV[] = Gurobi.Env()
-    GRBsetintparam(GRB_ENV[], "OutputFlag", 0)
-    GRBsetintparam(GRB_ENV[], "LogToConsole", 0)
-    GRBsetintparam(GRB_ENV[], "Threads", 0)
-    #GRBsetintparam(GRB_ENV[], "Method", 2)
-    #       mnist_19_local_21.vnnlib        mnist_18_local_18
-    #0 :    0.018826400587219343s/loop      0.03304489948205128s/loop
-    #1 :    0.01705984154058722s/loop       0.03352098044717949s/loop
-    #2 :    0.020955224224525042s/loop      0.038390683782564106s/loop
+    try
+        GRB_ENV[] = Gurobi.Env()
+        GRBsetintparam(GRB_ENV[], "OutputFlag", 0)
+        GRBsetintparam(GRB_ENV[], "LogToConsole", 0)
+        GRBsetintparam(GRB_ENV[], "Threads", 0)
+        #GRBsetintparam(GRB_ENV[], "Method", 2)
+        #       mnist_19_local_21.vnnlib        mnist_18_local_18
+        #0 :    0.018826400587219343s/loop      0.03304489948205128s/loop
+        #1 :    0.01705984154058722s/loop       0.03352098044717949s/loop
+        #2 :    0.020955224224525042s/loop      0.038390683782564106s/loop
+    catch e 
+        println("Gurobi error: ", e)
+        println("!!! falling back to GLPK !!!")
+        USE_GUROBI = false
+    end
 end
 
 #pinthreads(:cores)
@@ -56,8 +63,6 @@ using TimerOutputs
 const to = TimerOutput()
 
 using JuMP
-#using GLPK
-using Gurobi
 
 const GRB_ENV = Ref{Any}(nothing)
 

a5488d97db94246d8d3435749fbe0e88b4de44ec
Philipp Kern <philipp.kern@kit.edu>
2025-03-31 17:22:27 +0200

    construct zonotope from l, u and documentation for VerificationTask

--- src/Definitions.jl
+++ src/Definitions.jl
@@ -8,6 +8,30 @@ function Zonotope(G::GN,c::CN,influence) where {GN,CN}
     Zonotope(G, c, GN(influence))
 end
 
+
+"""Construct a zonotope from lower and upper bounds."""
+function Zonotope(low::AbstractVector{N}, high::AbstractVector{N}) where N<:Number
+    @assert length(low) == length(high)
+    mid = (low .+ high) ./ 2
+    distance = (high .- low) ./ 2
+    input_dim = length(low)
+    G = distance .* Matrix(I, input_dim, input_dim)[:, distance .> 0]
+    influence = Matrix(1.0I, sum(distance .> 0), sum(distance .> 0))
+    return Zonotope(G, mid, influence)
+end
+
+
+"""
+A verification task consists of an input specification and ...
+
+args:
+    middle - center of the input zonotope
+    distance - radius of the input zonotope (only non-zero dimensions)
+    distance_indices - indices of the non-zero dimensions
+    ∂Z - zonotope representing the distance between the two networks
+    verification_status - status of the verification task
+    distance_bound - ??? (why is this initialized to 1.0?)
+"""
 struct VerificationTask{N<:Number,GN<:AbstractMatrix{N},CN<:AbstractVector{N}}
     middle :: Vector{N}
     distance :: Vector{N}

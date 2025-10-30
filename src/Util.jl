fd4addf82fd015009a1ec422e48da72f585300a0
Philipp Kern <philipp.kern@kit.edu>
2025-10-30 14:15:44 +0100

    delete unused code

--- src/Util.jl
+++ src/Util.jl
@@ -5,19 +5,6 @@ function size(Z::Zonotope, d::Integer)
     return size(Z.G,d)
 end
 
-function truncate_network(T::Type,N::Network)
-    layers = []
-    for l in N.layers
-        if l isa ReLU
-            push!(layers, l)
-        elseif l isa Dense
-            push!(layers,Dense(convert.(Float64,convert.(T,deepcopy(l.W))),convert.(Float64,convert.(T,deepcopy(l.b)))))
-        else
-            throw("Unknown layer type")
-        end
-    end
-    return Network(layers)
-end
 
 function to_diff_zono(task :: VerificationTask)
     input_dim = size(task.middle,1)

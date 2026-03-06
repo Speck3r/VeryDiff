89ae31b6091a3643da043fea8aaf299a1b640cbe
Philipp Kern <philipp.kern@kit.edu>
2026-03-06 14:54:40 +0100

    difference transformer for Poly-Gelu

--- src/Transformers/Transformers.jl
+++ src/Transformers/Transformers.jl
@@ -9,7 +9,8 @@ using VNNLib.OnnxParser: ONNXLinear, ONNXRelu, ONNXAddConst, ONNXGelu
 using TimerOutputs
 
 import ..VeryDiff: zono_bounds, @simd_bool_expr
-import ..VeryDiff: islinear, REMEZ_ITERS, OPTIM_ITERS, approx_polynomial_lin, clenshaw_chebyshev, ChebyshevPolynomial, make_eval_poly, dpoly, real_roots
+import ..VeryDiff: islinear, remez, REMEZ_ITERS, OPTIM_ITERS, approx_polynomial_lin, clenshaw_chebyshev, ChebyshevPolynomial, make_eval_poly, dpoly, real_roots
+import ..VeryDiff: gelu, dgelu, d2gelu
 using VeryDiff
 
 using ..Definitions
@@ -17,6 +18,8 @@ using ..Debugger
 
 include("Util.jl")
 include("Init.jl")
+include("Gelu/GeluRelaxation.jl")
+include("Gelu/GeluDifferenceRelaxation.jl")
 include("Polynomial_Relaxations.jl")
 include("PolynomialDifferenceRelaxation.jl")
 include("Single_Transformers.jl")

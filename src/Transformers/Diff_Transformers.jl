b16063dddd771ce839fafe828852de0f92c7eb54
Philipp Kern <philipp.kern@kit.edu>
2026-03-06 13:24:30 +0100

    Poly-ReLU difference

--- src/Transformers/Diff_Transformers.jl
+++ src/Transformers/Diff_Transformers.jl
@@ -340,3 +340,217 @@ function propagate_layer!(
                     ifelse.(pos_any, .-μ.(lower₂,upper₂), 0.0)))
     end
 end
+
+
+function extract_bounds(Zin::DiffZonotope, bounds_cache :: Union{Nothing,BoundsCache}=nothing)
+    # Compute Bounds
+    bounds₁ = zono_bounds(Zin.Z₁)
+    bounds₂ = zono_bounds(Zin.Z₂)
+    ∂bounds = zono_bounds(Zin.∂Z)
+
+    if !bounds_cache.initialized
+        bounds_cache.lower₁ = copy(bounds₁[:,1])
+        bounds_cache.upper₁ = copy(bounds₁[:,2])
+        bounds_cache.lower₂ = copy(bounds₂[:,1])
+        bounds_cache.upper₂ = copy(bounds₂[:,2])
+        bounds_cache.∂lower = copy(∂bounds[:,1])
+        bounds_cache.∂upper = copy(∂bounds[:,2])
+        bounds_cache.initialized = true
+    else
+        bounds_cache.lower₁ .= max.(bounds₁[:,1], bounds_cache.lower₁)
+        bounds_cache.upper₁ .= min.(bounds₁[:,2], bounds_cache.upper₁)
+        bounds_cache.lower₂ .= max.(bounds₂[:,1], bounds_cache.lower₂)
+        bounds_cache.upper₂ .= min.(bounds₂[:,2], bounds_cache.upper₂)
+        bounds_cache.∂lower .= max.(∂bounds[:,1], bounds_cache.∂lower)
+        bounds_cache.∂upper .= min.(∂bounds[:,2], bounds_cache.∂upper)
+    end
+    lower₁ = bounds_cache.lower₁
+    upper₁ = bounds_cache.upper₁
+    lower₂ = bounds_cache.lower₂
+    upper₂ = bounds_cache.upper₂
+    ∂lower = bounds_cache.∂lower
+    ∂upper = bounds_cache.∂upper
+
+    @assert all(lower₁ .<= upper₁) "Invalid bounds for Z₁: lower bound is greater than upper bound"
+    @assert all(lower₂ .<= upper₂) "Invalid bounds for Z₂: lower bound is greater than upper bound"
+    @assert all(∂lower .<= ∂upper) "Invalid bounds for ∂Z: lower bound is greater than upper bound"
+
+    #@info "Bounds Cache: Z₁=[$(lower₁), $(upper₁)], Z₂=[$(lower₂), $(upper₂)], ∂Z=[$(∂lower), $(∂upper)]"
+    return lower₁, upper₁, lower₂, upper₂, ∂lower, ∂upper
+end
+
+
+"""
+    `extract_indices(ZoutRef, new_gen₁, new_gen₂, ∂new_gen)`
+
+    Extracts indices of common and owned generators for Z₁, Z₂ and ∂Z and allocates space for the output zonotope. 
+
+    args:
+    - `ZoutRef`: Reference to the output zonotope
+    - `new_gen₁`: Number of new generators owned by Z₁
+    - `new_gen₂`: Number of new generators owned by Z₂
+    - `∂new_gen`: Number of new generators owned by ∂Z
+
+    returns:
+    - `Zin`: Input zonotope
+    - `Zout`: Output zonotope with allocated space for new generators
+    - `pre_indices_Z₁`: Indices of generators of Zin.Z₁ in Zout.Z₁
+    - `pre_indices_Z₂`: Indices of generators of Zin.Z₂ in Zout.Z₂
+    - `pre_indices₁`: Indices of generators of Zin.Z₁ in Zout.∂Z
+    - `pre_indices₂`: Indices of generators of Zin.Z₂ in Zout.∂Z
+    - `post_indices₁`: Indices of generators of Zout.Z₁ in Zout.∂Z
+    - `post_indices₂`: Indices of generators of Zout.Z₂ in Zout.∂Z
+"""
+function extract_generator_indices(Zin::DiffZonotope, ZoutRef::CachedZonotope, new_gen₁::Integer, new_gen₂::Integer, ∂new_gen::Integer)
+    # @debug "Instable Neurons: Network 1: $new_gen₁, Network 2: $new_gen₂, Differential: $∂new_gen"
+    Zout_proto = ZoutRef.zonotope_proto # Need this to be able to access the generator ids
+    gen_sizes₁ = zeros(Int64,length(Zout_proto.Z₁.generator_ids))
+    gen_sizes₂ = zeros(Int64,length(Zout_proto.Z₂.generator_ids))
+    ∂gen_sizes = zeros(Int64,length(Zout_proto.∂Z.generator_ids))
+
+    pre_indices_Z₁ = intersect_indices(Zout_proto.Z₁.generator_ids, Zin.Z₁.generator_ids)
+    pre_indices_Z₂ = intersect_indices(Zout_proto.Z₂.generator_ids, Zin.Z₂.generator_ids)
+    pre_indices₁ = intersect_indices(Zout_proto.∂Z.generator_ids, Zin.Z₁.generator_ids)
+    pre_indices₂ = intersect_indices(Zout_proto.∂Z.generator_ids, Zin.Z₂.generator_ids)
+    ∂pre_indices = intersect_indices(Zout_proto.∂Z.generator_ids, Zin.∂Z.generator_ids)
+
+    for (i, idx) in enumerate(pre_indices_Z₁)
+        gen_sizes₁[idx] = size(Zin.Z₁.Gs[i],2)
+    end
+    gen_sizes₁[Zout_proto.Z₁.owned_generators] += new_gen₁
+    for (i, idx) in enumerate(pre_indices_Z₂)
+        gen_sizes₂[idx] = size(Zin.Z₂.Gs[i],2)
+    end
+    gen_sizes₂[Zout_proto.Z₂.owned_generators] += new_gen₂
+    # This mayoverwrite sizes, but columns should be consistent
+    # TODO(steuber): Can we make this cleaner?
+    for (i, idx) in enumerate(∂pre_indices)
+        # @info "Setting ∂Z generator $idx size to $(size(Zin.∂Z.Gs[i],2)) (from ∂Z)"
+        ∂gen_sizes[idx] = size(Zin.∂Z.Gs[i],2)
+    end
+    for (i, idx) in enumerate(pre_indices₁)
+        # @info "Setting ∂Z generator $idx size to $(size(Zin.Z₁.Gs[i],2)) (from Z₁)"
+        ∂gen_sizes[idx] = size(Zin.Z₁.Gs[i],2)
+    end
+    for (i, idx) in enumerate(pre_indices₂)
+        # @info "Setting ∂Z generator $idx size to $(size(Zin.Z₂.Gs[i],2)) (from Z₂)"
+        ∂gen_sizes[idx] = size(Zin.Z₂.Gs[i],2)
+    end
+    # @info "Generator sizes before new gens: Z₁=$(gen_sizes₁), Z₂=$(gen_sizes₂), ∂Z=$(∂gen_sizes)"
+    ∂old_gen = ∂gen_sizes[Zout_proto.∂Z.owned_generators]
+    ∂gen_sizes[Zout_proto.∂Z.owned_generators] += ∂new_gen
+    # Find idx of generators owned by Z₁ and Z₂ in ∂Z
+    idx1 = find_index_position(Zout_proto.∂Z.generator_ids, Zout_proto.Z₁.generator_ids[Zout_proto.Z₁.owned_generators])
+    idx2 = find_index_position(Zout_proto.∂Z.generator_ids, Zout_proto.Z₂.generator_ids[Zout_proto.Z₂.owned_generators])
+    ∂gen_sizes[idx1] += new_gen₁
+    ∂gen_sizes[idx2] += new_gen₂
+    Zout_proto = nothing # Avoid missuse
+    # @info "ReLU DiffZonotope Generators: Z₁=$(gen_sizes₁), Z₂=$(gen_sizes₂), ∂Z=$(∂gen_sizes)"
+    Zout = get_zonotope!(ZoutRef, gen_sizes₁, gen_sizes₂, ∂gen_sizes)
+    post_indices₁ = intersect_indices(Zout.∂Z.generator_ids, Zout.Z₁.generator_ids)
+    post_indices₂ = intersect_indices(Zout.∂Z.generator_ids, Zout.Z₂.generator_ids)
+    
+    return Zout, pre_indices_Z₁, pre_indices_Z₂, pre_indices₁, pre_indices₂, ∂pre_indices, post_indices₁, post_indices₂, ∂old_gen
+end
+
+
+function add_∂error_terms!(Z, mask, ϵs, dim, ∂old_gen, offset)
+    # Add new generators from c
+    c_pos = findall(mask)
+    A = Z.∂Z.Gs[Z.∂Z.owned_generators]
+    @inbounds for i in 1:length(c_pos)
+        row = c_pos[i]
+        col = ∂old_gen + offset + i
+        if mask[row]
+            A[row, col] = ϵs[i]
+        end
+    end
+
+    # TODO: use length of c_pos in offset of next call
+    return offset + length(c_pos)
+end
+
+
+function propagate_layer!(
+    ZoutRefVec :: Vector{CachedZonotope},
+    Ls :: DiffLayer{
+        <:ONNXPoly{S1,N},
+        <:ONNXPoly{S2},
+        <:ONNXRelu{S3}},
+    inputs :: Vector{DiffZonotope};
+    bounds_cache :: Union{Nothing,BoundsCache}=nothing) where {N,S1, S2, S3}
+    @assert length(inputs) == 1 "Activation layer should have exactly one input zonotope"
+    @assert length(ZoutRefVec) == 1 "Dense layer should have exactly one output zonotope"
+    ZoutRef = ZoutRefVec[1]
+    Zin = inputs[1]
+
+    @assert !isnothing(bounds_cache)
+    lower₁, upper₁, lower₂, upper₂, ∂lower, ∂upper = extract_bounds(Zin, bounds_cache)
+
+    neg, pos, unstable = relu_stability_mask(lower₂, upper₂)
+    new_gen₁ = length(lower₁)
+    new_gen₂ = count(unstable)
+    # for neg, we don't need new generators, we can just pass through the zonotope for the polynomial network
+    ∂new_gen = count(pos) + count(unstable)
+
+    Zout, pre_indices_Z₁, pre_indices_Z₂, pre_indices₁, pre_indices₂, ∂pre_indices, post_indices₁, post_indices₂, ∂old_gen = extract_generator_indices(Zin, ZoutRef, new_gen₁, new_gen₂, ∂new_gen)
+
+    L1 = get_layer1(Ls)
+    L2 = get_layer2(Ls)
+    # Compute Zonotopes for individual networks
+    propagate_layer!(Zout.Z₁, L1, Zin.Z₁;lower=lower₁, upper=upper₁)
+    propagate_layer!(Zout.Z₂, L2, Zin.Z₂;lower=lower₂, upper=upper₂)
+
+    if VeryDiff.USE_DIFFZONO[]
+        dim = length(lower₁)
+
+        # Reset to zero
+        Zout.∂Z.c .= 0.0
+        for g in Zout.∂Z.Gs
+            g[neg, :] .= 0.0
+        end
+
+        #############
+        ##   Neg   ##
+        #############
+        # p(x) - ReLU(y) = p(x) -> just take the zonotope for the first network
+        updateGenerators!(Zout.∂Z.Gs, post_indices₁, Zout.Z₁.Gs, neg)
+        Zout.∂Z.c[neg] .= @view Zout.Z₁.c[neg]
+
+        #############
+        ##   Pos   ##
+        #############
+        # with Δ = x - y, we know that y = x - Δ
+        # thus p(x) - ReLU(y) = p(x) - ReLU(x - Δ) = p(x) - (x - Δ) = (p(x) - x) + Δ
+        # ideally, the range of Δ is much smaller than the range of y, 
+        # while the range of p(x) - x is not much worse than the range of p(x)
+        # α*x + β - γ ≤ p(x) - x ≤ α*x + β + γ
+        α, β, γ = poly_pos_approx(L1, pos, lower₁, upper₁)
+        # Δ' = α*x + β + Δ ± γ
+        updateGeneratorsMul!(Zout.∂Z.Gs, pre_indices₁, Zin.Z₁.Gs, α, pos)  # ∂Z += α * Z₁
+        updateGeneratorsAdd!(Zout.∂Z.Gs, ∂pre_indices, Zin.∂Z.Gs, pos)     # ∂Z += ∂Z
+        Zout.∂Z.c[pos] .= α .* (@view Zin.Z₁.c[pos]) .+ β .+ (@view Zin.∂Z.c[pos]) 
+        offset = 0
+        offset = add_∂error_terms!(Zout, pos, γ, dim, ∂old_gen, offset)
+
+        ##################
+        ##   Unstable   ##
+        ##################
+        # Unstable:
+        a_init, b_init = poly_initial_slope_guess(lower₁, upper₁, lower₂, upper₂, ∂lower, ∂upper)
+
+        # why is there no better way to handle broadcasted tuples?
+        # TODO: dispatch on L1,L2 not just on L1, so we can have other types of combinations
+        res = find_good_poly_diff_approx(L1, unstable, lower₁, upper₁, ∂lower, ∂upper, a_init, b_init)
+        a = getindex.(res, 1)  # slope of x
+        b = getindex.(res, 2)  # slope of Δ 
+        c = getindex.(res, 3)  # bias 
+        ϵ = getindex.(res, 4)  # new error term
+
+        # a*x + b*Δ + c ± ϵ
+        updateGeneratorsMul!(Zout.∂Z.Gs, pre_indices₁, Zin.Z₁.Gs, a, unstable)  # ∂Z += a * Z₁
+        updateGeneratorsAddMul!(Zout.∂Z.Gs, ∂pre_indices, Zin.∂Z.Gs, b, unstable)  # ∂Z += b * ∂Z
+        Zout.∂Z.c[unstable] .= a .* (@view Zin.Z₁.c[unstable]) .+ b .* (@view Zin.∂Z.c[unstable]) .+ c
+        offset = add_∂error_terms!(Zout, unstable, ϵ, dim, ∂old_gen, offset)
+    end
+end

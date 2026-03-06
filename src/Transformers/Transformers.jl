b076a4eb4721803d3d6b6b812a50dd4b8a9d644d
Philipp Kern <philipp.kern@kit.edu>
2026-03-06 13:25:51 +0100

    include files in Transformers.jl

--- src/Transformers/Transformers.jl
+++ src/Transformers/Transformers.jl
@@ -1,6 +1,7 @@
 module Transformers
 
 using LinearAlgebra
+using Optim
 
 using VNNLib
 using VNNLib.OnnxParser: ONNXLinear, ONNXRelu, ONNXAddConst, ONNXGelu
@@ -8,6 +9,7 @@ using VNNLib.OnnxParser: ONNXLinear, ONNXRelu, ONNXAddConst, ONNXGelu
 using TimerOutputs
 
 import ..VeryDiff: zono_bounds, @simd_bool_expr
+import ..VeryDiff: islinear, REMEZ_ITERS, OPTIM_ITERS, approx_polynomial_lin, clenshaw_chebyshev, ChebyshevPolynomial, make_eval_poly, dpoly, real_roots
 using VeryDiff
 
 using ..Definitions
@@ -15,6 +17,8 @@ using ..Debugger
 
 include("Util.jl")
 include("Init.jl")
+include("Polynomial_Relaxations.jl")
+include("PolynomialDifferenceRelaxation.jl")
 include("Single_Transformers.jl")
 include("Diff_Transformers.jl")
 include("Network.jl")

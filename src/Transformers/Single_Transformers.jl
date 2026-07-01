56e24eab90ba46659236e5618352af34fe39000c
Jonas Specker <uzisa@student.kit.edu>
2026-07-01 12:41:11 +0200

    raven single sigmoid transformer

--- src/Transformers/Single_Transformers.jl
+++ src/Transformers/Single_Transformers.jl
@@ -161,161 +161,48 @@ function propagate_layer!(ZoutRef :: Zonotope, L :: Union{ONNXPoly{S}, ONNXGelu{
     end
 end
 
-function propagate_layer!(ZoutRefVec :: Vector{Zonotope}, L :: ONNXSigmoid{S}, inputs :: Vector{Zonotope}; lower=nothing, upper=nothing) where {S}
-    @assert length(inputs) == 1 "Sigmoid layer should have exactly one input"
-    @assert length(ZoutRefVec) == 1 "Sigmoid layer should have exactly one output"
-    ZoutRef = ZoutRefVec[1]
-    Zin = inputs[1]
-    return propagate_layer!(ZoutRef, L, Zin; lower=lower, upper=upper)
-end
-
-function σ(x)
-    positive = (x .> 0.0)
-    negative = .!positive
-    result = zeros(length(x))
-    result[positive] .= (1 ./ (1 .+ exp.(.- x[positive])))
-    result[negative] .= (exp.(x[negative]) ./ (exp.(x[negative]) .+ 1))
-    return result
-end
-
-function σ´(x)
-    return σ(x) .* (1 .- σ(x))
-end
-
-function solve_σ´(λ, use_upper)
-    if any(λ .== 0)
-        throw("λ is zero")
-    end
-    if use_upper
-        return log.((1 .- 2 .* λ  .+ sqrt.(1 .- 4 .* λ)) ./ (2 .* λ))
-    else
-        return log.((1 .- 2 .* λ .- sqrt.(1 .- 4 .* λ)) ./ (2 .* λ))
-    end
-end
-
-function fsecant_slope(lower, upper)
-    secant_slope = clamp.((σ(upper) .- σ(lower)) ./ (upper .- lower), 0, 0.25)
-    secant_slope[secant_slope .< CUTOFF_SIGMOID_SLOPE] .= 0
-    return secant_slope
-end
-
-function iterate_tagent_point(start, fix_point, use_upper)
-    tangent_point = start
-    solve_derivative = trues(length(start))
-    slope = zeros(length(start))
-    for i in 1:LOOP_ITERATIONS_SINGLE_SIGMOID
-        slope[solve_derivative] .= fsecant_slope(tangent_point[solve_derivative], fix_point[solve_derivative])
-        solve_derivative .= (slope .>= CUTOFF_SIGMOID_SLOPE)
-        tangent_point[solve_derivative] .= solve_σ´(slope[solve_derivative], use_upper)
-        tangent_point[.!solve_derivative] .= start[.!solve_derivative]
-    end
-    return tangent_point
-end
-
-function propagate_layer!(ZoutRef :: Zonotope, _L :: ONNXSigmoid{S}, Zin :: Zonotope; lower=nothing, upper=nothing) where {S}
+function propagate_layer!(ZoutRef :: Zonotope, L :: ONNXSigmoid{S}, Zin :: Zonotope; lower=nothing, upper=nothing) where {S}
     if isnothing(lower) || isnothing(upper)
         bounds = zono_bounds(Zin)
         lower = @view bounds[:,1]
         upper = @view bounds[:,2]
     end
-    
-    #println("input_lower = $(lower)")
-    #println("input_upper = $(upper)")
 
-    dim = length(lower)
-    new_gens = dim
-    only_center = (lower .== upper) # handling of this case can be optimised
+    λ, β, γ = get_linear_relaxation(L, lower, upper)
 
-    #setup data structures
-    iterative_slope_lower = zeros(dim)
-    iterative_slope_upper = zeros(dim)
-    iterative_slope = zeros(dim)
-    tangent_points = zeros(2, dim) #[lower tangent points ,upper tangent points]
+    ZoutRef.c .= λ .* Zin.c .+ β
 
-    #calc secant slope for all 
-    secant_slope = fsecant_slope(lower, upper)
-
-    #check secant slope vailidity
-    upper_derivative = σ´(upper)
-    lower_derivative = σ´(lower)
-    secant_upper = @simd_bool_expr dim (secant_slope <= upper_derivative)
-    secant_lower = @simd_bool_expr dim (secant_slope <= lower_derivative)
-
-    #calc tangent points and slope iteratively
-    mask_iteration = (.!(secant_upper .|| secant_lower))
-    tangent_points[1, mask_iteration] = iterate_tagent_point(lower[mask_iteration], upper[mask_iteration], false) 
-    tangent_points[2, mask_iteration] = iterate_tagent_point(upper[mask_iteration], lower[mask_iteration], true)
-    iterative_slope_lower[mask_iteration] = σ´(tangent_points[1, mask_iteration])
-    iterative_slope_upper[mask_iteration] = σ´(tangent_points[2, mask_iteration])
-
-    #choose smaller iterative slope
-    mask_lower = @simd_bool_expr dim (iterative_slope_lower <= iterative_slope_upper)
-    mask_upper = @simd_bool_expr dim (iterative_slope_lower > iterative_slope_upper)
-    iterative_slope[mask_lower] = iterative_slope_lower[mask_lower]
-    iterative_slope[mask_upper] = iterative_slope_upper[mask_upper]
-
-    #set tangent points where opposite slope is used using σ'(x) = σ'(-x)
-    tangent_points[1, mask_upper] = .-tangent_points[2, mask_upper]
-    tangent_points[2, mask_lower] = .-tangent_points[1, mask_lower] 
-
-    #set tangent points where secant slope is used
-    secant_slope_zero = (secant_slope .== 0) 
-    tangent_points[1, secant_upper .&& .!secant_slope_zero] = solve_σ´(secant_slope[secant_upper .&& .!secant_slope_zero], false)
-    tangent_points[1, secant_upper .&& secant_slope_zero] = lower[secant_upper .&& secant_slope_zero]
-    tangent_points[1, secant_lower] = upper[secant_lower]
-    tangent_points[2, secant_upper] = upper[secant_upper]
-    tangent_points[2, secant_lower .&& .!secant_slope_zero] = solve_σ´(secant_slope[secant_lower .&& .!secant_slope_zero], true)
-    tangent_points[2, secant_lower .&& secant_slope_zero] = upper[secant_lower .&& secant_slope_zero]
-
-    #calc final slope for all
-    λ = ifelse.(secant_upper .|| secant_lower, secant_slope, iterative_slope)
-
-    #calc and apply center offset
-    lower_tangent_points = @view tangent_points[1, :]
-    upper_tangent_points = @view tangent_points[2, :]
-    σ_upper = σ(upper_tangent_points)
-    σ_lower = σ(lower_tangent_points)
-    
-    ν = 0.5 .* (.-λ .* upper_tangent_points .+ σ_upper .+ 1e-7 .- λ .* lower_tangent_points .+ σ_lower .- 1e-7)
-    ZoutRef.c .= λ .* Zin.c .+ ν
-    ZoutRef.c[only_center] .= σ(Zin.c[only_center])
-
-    #calc new generator
-    μ = 0.5 .* (.-λ .* upper_tangent_points .+ σ_upper .+ 1e-7 .+ λ .* lower_tangent_points .- σ_lower .+ 1e-7)
-    μ[only_center] .= 0 # one additional column per only center affine form 
-    
+    new_gens = size(lower, 1)
 
     indices = intersect_indices(ZoutRef.generator_ids, Zin.generator_ids)
     if VeryDiff.NEW_HEURISTIC[]
         influence_new = ZoutRef.influence
         column_pos = size(influence_new[ZoutRef.owned_generators],2) - new_gens + 1
+        # @debug "Adding $new_gens new columns at position $column_pos to influence matrix of owned generator ID $(ZoutRef.generator_ids[ZoutRef.owned_generators])"
+        # @debug "Sizes of influence matrices: $([size(inf) for inf in Zin.influence])"
+        # Other influence matrices remain the same
+        # Only need to update the owned generator influence matrix
         if !isnothing(Zin.owned_generators) && Zin.owned_generators == attempt_find_index_position(Zin.generator_ids, ZoutRef.generator_ids[ZoutRef.owned_generators])
             influence_new[ZoutRef.owned_generators][:, 1:column_pos-1] .= Zin.influence[Zin.owned_generators]
         end
+        # @debug "Size of owned influence matrix after copy: $(size(influence_new[ZoutRef.owned_generators]))"
         influence_new[ZoutRef.owned_generators][:,column_pos:end] .= 0.0
         bounds_range = upper .- lower
         @inbounds for (idx, g) in enumerate(Zin.Gs)
-            influence_new[ZoutRef.owned_generators][:,column_pos:end] .+= Zin.influence[idx] * abs.((g) ./ bounds_range)'
+            influence_new[ZoutRef.owned_generators][:,column_pos:end] .+= Zin.influence[idx] * abs.(g ./ bounds_range)'
         end
     else
         influence_new = Zin.influence
     end
 
-    num_new_gens = dim
+    updateGeneratorsMul!(ZoutRef.Gs, indices, Zin.Gs, λ, :)
 
-    λ[only_center] .= 1
-    updateGeneratorsMul!(ZoutRef.Gs, indices, Zin.Gs, λ, :) #apply slope
-    ZoutRef.Gs[ZoutRef.owned_generators][:,(end-num_new_gens+1):end] .= 0.0
-    generator_offset = size(ZoutRef.Gs[ZoutRef.owned_generators],2) - num_new_gens
+    # fill number of new_gens columns with all 0, 
+    # then add the error term to the diagonal of these columns
+    ZoutRef.Gs[ZoutRef.owned_generators][:,(end-new_gens+1):end] .= 0.0
+    generator_offset = size(ZoutRef.Gs[ZoutRef.owned_generators],2) - new_gens
     A = ZoutRef.Gs[ZoutRef.owned_generators]
-    @inbounds for row in axes(A, 1)
-        A[row, (generator_offset + row)] = abs(μ[row]) #add new generators
+    @inbounds for (i, row) in enumerate(1:new_gens)
+        A[row, (generator_offset + i)] = abs(γ[row])
     end
-
-    #output_bounds = zono_bounds(ZoutRef)
-    #output_lower = @view output_bounds[:,1]
-    #output_upper = @view output_bounds[:,2]
-    #println("output_lower = $(output_lower)")
-    #println("output_upper = $(output_upper)")
-
 end
